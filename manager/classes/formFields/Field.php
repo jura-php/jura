a80ab19e942969e29b11005befd09969543cd729
Ramon Fritsch <ramon@joy-interactive.com>
2013-07-22 16:23:28 -0300

    Validação

--- manager/classes/formFields/Field.php
+++ manager/classes/formFields/Field.php
@@ -8,6 +8,10 @@ class Field
 
 	public $defaultValue;
 
+	public $required;
+	public $validationPattern;
+	public $validationTitle;
+
 	public function __construct($name, $label = null)
 	{
 		$this->name = $name;
@@ -15,6 +19,10 @@ class Field
 		$this->flags = "";
 		$this->type = "text";
 		$this->defaultValue = "";
+
+		$this->required = true;
+		$this->validationPattern = ".*";
+		$this->validationTitle = "Preencha o campo"; //TODO: Usar linguagem
 	}
 
 	public function hasFlag($flag)
@@ -31,6 +39,13 @@ class Field
 		$config["label"] = $this->label;
 		$config["flags"] = $this->flags;
 
+		$validation = array();
+		$validation["required"] = $this->required;
+		$validation["pattern"] = $this->validationPattern;
+		$validation["title"] = $this->validationTitle;
+
+		$config["validation"] = $validation;
+
 		return $config;
 	}
 
@@ -42,6 +57,11 @@ class Field
 	public function unformat($value)
 	{
 		return $value;
+	}
+
+	public function validation($type)
+	{
+		
 	}
 }
 ?>
