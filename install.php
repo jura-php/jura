7fcb89dce4aab6c6d921db984af1928d8d47e8bd
Ramon Fritsch <ramon@joy-interactive.com>
2013-08-19 11:16:55 -0300

    Update install.php

--- install.php
+++ install.php
@@ -125,6 +125,24 @@ make_file("../deploy.sh", "chmod -Rf 777 app/storage/", function () {
 	}
 });
 
+make_file("../index.php", '<?php
+
+define("DS", DIRECTORY_SEPARATOR);
+define("EXT", ".php");
+define("CRLF", PHP_EOL);
+
+define("J_START", microtime(true));
+define("J_PATH", realpath(__DIR__) . DS);
+define("J_APPPATH", realpath(__DIR__)  . DS . "app" . DS);
+define("J_MANAGERPATH", realpath(__DIR__)  . DS . "manager" . DS);
+define("J_SYSTEMPATH", realpath(__DIR__)  . DS . "system" . DS);
+define("J_LOCAL_ENV", "local");
+define("J_PREVIEW_ENV", "preview");
+
+require J_SYSTEMPATH . "core" . DS . "core" . EXT;
+
+?>');
+
 make_file("../app/config/routes.php");
 
 make_file("../app/config/application.php", function () {
