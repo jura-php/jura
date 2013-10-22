f2958f6c301039a9a2482ae6d7c2ed54a3ba677b
Ramon Fritsch <ramon@joy-interactive.com>
2013-10-22 13:52:34 -0200

    ajuste campo textarea para herdar campo text

--- manager/classes/formFields/TextAreaField.php
+++ manager/classes/formFields/TextAreaField.php
@@ -1,28 +1,12 @@
 <?php
-class TextAreaField extends Field
+class TextAreaField extends TextField
 {
-	public $listLimit = 100;
-
 	public function __construct($name, $label = null)
 	{
 		parent::__construct($name, $label);
 
 		$this->type = "textarea";
-	}
-
-	public function validation($type)
-	{
-		$this->validationLength = 65535;
-	}
-
-	public function format($value)
-	{
-		if ($this->module->flag == "L")
-		{
-			return Str::limit($value, $this->listLimit);
-		}
-
-		return $value;
+		$this->validation("textarea");
 	}
 }
 ?>
